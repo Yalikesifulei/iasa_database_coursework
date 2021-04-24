ae1c5347ab4bcc53d45d8bef339233eac5d63f3a
Oleksii Galganov <alex.galganov@gmail.com>
2021-04-25 00:05:25 +0300

    template update

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 import pymysql
 from db_connect import db
 
@@ -8,70 +8,18 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-tname = 'subjects'
-@app.route('/chairs')
-def chairs():
-    cursor = db.cursor()
-    sql = f"select * from `chairs`;"
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    return render_template('chairs.html', results=results)
+tables = ['chairs', 'faculties', 'groups', 'session', 
+          'students', 'subjects', 'teachers', 'teachers_subjects']
 
-@app.route('/faculties')
-def faculties():
-    cursor = db.cursor()
-    sql = f"select * from `faculties`;"
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    return render_template('faculties.html', results=results)
-
-@app.route('/groups')
-def groups():
-    cursor = db.cursor()
-    sql = f"select * from `groups`;"
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    return render_template('groups.html', results=results)
-
-@app.route('/session')
-def session():
-    cursor = db.cursor()
-    sql = f"select * from `session`;"
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    return render_template('session.html', results=results)
-
-@app.route('/students')
-def students():
-    cursor = db.cursor()
-    sql = f"select * from `students`;"
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    return render_template('students.html', results=results)
-
-@app.route('/subjects')
-def subjects():
-    cursor = db.cursor()
-    sql = f"select * from `subjects`;"
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    return render_template('subjects.html', results=results)
-
-@app.route('/teachers')
-def teachers():
-    cursor = db.cursor()
-    sql = f"select * from `teachers`;"
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    return render_template('teachers.html', results=results)
-
-@app.route('/teachers_subjects')
-def teachers_subjects():
-    cursor = db.cursor()
-    sql = f"select * from `teachers_subjects`;"
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    return render_template('teachers_subjects.html', results=results)
+@app.route('/tables/<string:table_name>')
+def select_table(table_name):
+    if table_name not in tables:
+        abort(404)
+    with db.cursor() as cursor:
+        cursor.execute(f'select * from `{table_name}`')
+        columns = [desc[0] for desc in cursor.description]
+        records = cursor.fetchall()
+    return render_template('table.html', table=records, columns=columns)
 
 if __name__ == "__main__":
     app.run()
